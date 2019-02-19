6ab4a565e7fc0dd56fd9dc579be55401baf0c302
Constantin Pape <c.pape@gmx.net>
2019-02-19 21:05:06 +0100

    Add reference image

---
+++ generate_data/generate_reference_image.py
@@ -0,0 +1,12 @@
+from skimage.data import astronaut
+from skimage.io import imsave
+
+
+def write_reference_image():
+    path = '../data/reference_image.png'
+    im = astronaut()
+    imsave(path, im)
+
+
+if __name__ == '__main__':
+    write_reference_image()
