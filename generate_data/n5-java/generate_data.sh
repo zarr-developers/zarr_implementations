6c9679169ef7e470ebffb80dd90d22ee36c11ccd
Constantin Pape <c.pape@gmx.net>
2019-02-20 14:50:09 +0100

    Add n5-java data

--- generate_data/n5-java/generate_data.sh
+++ generate_data/n5-java/generate_data.sh
@@ -1,3 +1,2 @@
 mvn clean package
-java -jar target/n5_java-1.0.0.jar
-# java -cp target/n5_java-1.0.0.jar zarr_implementations.n5_java.App
+java -cp target/n5_java-1.0.0.jar zarr_implementations.n5_java.App
