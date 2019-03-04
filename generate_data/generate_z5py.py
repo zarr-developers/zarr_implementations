b0f795c926b3ceca76eab0a86f0406ad81f3df6f
Constantin Pape <c.pape@gmx.net>
2019-03-04 19:13:03 +0100

    Update zarr comporession

--- generate_data/generate_z5py.py
+++ generate_data/generate_z5py.py
@@ -4,15 +4,22 @@ from skimage.data import astronaut
 # choose chunks s.t. we do have overhanging edge-chunks
 CHUNKS = (100, 100, 1)
 
+# options for the different compressors
+COMPRESSION_OPTIONS = {'blosc': {'filter_id': 'lz4'}}
 
+
+# TODO support more compressors:
+# - more compressors in numcodecs
+# - more blosc codecs
 def generate_zarr_format(compressors=['gzip', 'blosc', 'zlib', 'raw']):
     path = '../data/z5py.zr'
     im = astronaut()
 
     f = z5py.File(path)
     for compressor in compressors:
-        name = compressor
-        f.create_dataset(name, data=im, compression=compressor, chunks=CHUNKS)
+        copts = COMPRESSION_OPTIONS.get(compressor, {})
+        name = compressor if compressor != 'blosc' else '%s/%s' % (compressor, copts.get('filter_id'))
+        f.create_dataset(name, data=im, compression=compressor, chunks=CHUNKS, **copts)
 
 
 def generate_n5_format(compressors=['gzip', 'raw']):
