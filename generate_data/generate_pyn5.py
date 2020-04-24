a6b1c1975f40d034a8ce81c957462d6ff771d7a8
Chris Barnes <barnesc@janelia.hhmi.org>
2020-04-24 21:22:36 +0100

    data generation script for pyn5

---
+++ generate_data/generate_pyn5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from pathlib import Path
+import pyn5
+from skimage.io import imread
+
+# choose chunks s.t. we do have overhanging edge-chunks
+CHUNKS = (100, 100, 1)
+
+
+def generate_n5_format(compressors=pyn5.CompressionType):
+    here = Path(__file__).resolve().parent
+    data_dir = here.parent / "data"
+    path = data_dir / "pyn5.n5"
+
+    im = imread(data_dir / "reference_image.png")
+
+    f = pyn5.File(path, pyn5.Mode.CREATE_TRUNCATE)
+    for compressor in compressors:
+        name = str(compressor)
+        f.create_dataset(name, data=im, chunks=CHUNKS, compression=compressor)
+
+
+if __name__ == '__main__':
+    generate_n5_format()
