f9973828e0922b39470f494aa0acf53c7d6b4be5
Constantin Pape <c.pape@gmx.net>
2020-04-27 14:09:02 +0200

    Update truncate in n5 and pin pillow version

--- generate_data/n5-java/generate_data.sh
+++ generate_data/n5-java/generate_data.sh
@@ -1,10 +1,10 @@
-# clean the n5 data. ideally, this would be done in n5-java, but I don't know if
-# it supports truncate mode
-rm -r data/n5-java.n5
-
 # cd to this directory
 # https://stackoverflow.com/a/6393573/2700168
 cd "${0%/*}"
+
+# clean the n5 data. ideally, this would be done in n5-java, but I don't know if
+# it supports truncate mode
+rm -r ../../data/n5-java.n5
 
 mvn clean package
 java -cp target/n5_java-1.0.0.jar zarr_implementations.n5_java.App
