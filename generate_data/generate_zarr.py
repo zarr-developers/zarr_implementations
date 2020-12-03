404bf3b570f32516691ad4f5ff3e0a7951c0fbc5
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-12-03 13:57:23 -0800

    filter-id -> codec

--- generate_data/generate_zarr.py
+++ generate_data/generate_zarr.py
@@ -4,10 +4,12 @@ from skimage.data import astronaut
 
 # choose chunks s.t. we do have overhanging edge-chunks
 CHUNKS = (100, 100, 1)
-STR_TO_COMPRESSOR = {'gzip': numcodecs.GZip,
-                     'blosc': numcodecs.Blosc,
-                     'zlib': numcodecs.Zlib}
-COMPRESSION_OPTIONS = {'blosc': {'filter_id': 'lz4'}}
+STR_TO_COMPRESSOR = {
+    "gzip": numcodecs.GZip,
+    "blosc": numcodecs.Blosc,
+    "zlib": numcodecs.Zlib,
+}
+COMPRESSION_OPTIONS = {"blosc": {"codec": "lz4"}}
 
 
 # TODO use more compressors from numcodecs and more blosc filter_ids
@@ -19,9 +21,9 @@ def generate_zarr_format(compressors=['gzip', 'blosc', 'zlib', None]):
     for compressor in compressors:
         copts = COMPRESSION_OPTIONS.get(compressor, {})
         if compressor is None:
-            name = 'taw'
-        elif compressor == 'blosc':
-            name = '%s/%s' % (compressor, copts.get('filter_id'))
+            name = "taw"
+        elif compressor == "blosc":
+            name = "%s/%s" % (compressor, copts.get("codec"))
         else:
             name = compressor
         compressor_impl = STR_TO_COMPRESSOR[compressor](**copts) if compressor is not None else None
